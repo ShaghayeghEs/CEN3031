7f6bbc7e745a251dbadecfee59423775876c843f
kapooramanpreet <kapoorise@gmail.com>
2017-08-28 20:49:32 -0400

    Discussion 1 - To Download

--- Discussion1_ToDo/ToDo.js
+++ Discussion1_ToDo/ToDo.js
@@ -60,33 +60,33 @@
 
 //Nineth Task
 
-angular.module('todoApp', [])
-.controller('TodoListController', function() {
-  var todoList = this;
-  todoList.taskAndStatus = [
-    {text:'Some Task 1', done:true},
-    {text:'Some Task 2', done:false}
-  ];
+// angular.module('todoApp', [])
+// .controller('TodoListController', function() {
+//   var todoList = this;
+//   todoList.taskAndStatus = [
+//     {text:'Some Task 1', done:true},
+//     {text:'Some Task 2', done:false}
+//   ];
 
-  todoList.addTask =function(){
-    todoList.taskAndStatus.push({text:todoList.thisBoxInput, done:false});
-  };
+//   todoList.addTask =function(){
+//     todoList.taskAndStatus.push({text:todoList.thisBoxInput, done:false});
+//   };
 
-  todoList.deleteCompletedTask=function(){
-    var oldList=todoList.taskAndStatus;
-    todoList.taskAndStatus=[];
-    angular.forEach(oldList, function(obj) {
-              if (!obj.done) todoList.taskAndStatus.push(obj);
-            });
-  };
+//   todoList.deleteCompletedTask=function(){
+//     var oldList=todoList.taskAndStatus;
+//     todoList.taskAndStatus=[];
+//     angular.forEach(oldList, function(obj) {
+//               if (!obj.done) todoList.taskAndStatus.push(obj);
+//             });
+//   };
 
-  todoList.countTask=function() {
-    var count = 0;
-    angular.forEach(todoList.taskAndStatus, function(obj) {
-      count += obj.done ? 0 : 1;
-    });
-    return count;
-  };
+//   todoList.countTask=function() {
+//     var count = 0;
+//     angular.forEach(todoList.taskAndStatus, function(obj) {
+//       count += obj.done ? 0 : 1;
+//     });
+//     return count;
+//   };
 
-});
+// });
  
